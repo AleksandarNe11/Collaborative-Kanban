58a9ffa5b346fe3ef4fd58bc367ee12167a3f165
Rufael Musa <rufaelmusa@gmail.com>
2022-03-25 09:45:25 -0400

    Update Join.js

--- src/app/landingpage/Join.js
+++ src/app/landingpage/Join.js
@@ -2,6 +2,5 @@ function Join_Room() {
     console.log("TEST");
     let ID = document.getElementById("boardID");
     let rm = ID?.innerText;
-    console.log("HEEELP");
     //document.getElementById("JOIN").onclick = "location.href='//localhost:4200/${ID}'";
   }
